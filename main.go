27dedab56d5b74fe9b10cab4945e57d499b7dde1
azurata <git@azurata.me>
2021-12-13 10:09:56 +0900

    feat: Load games json file

--- main.go
+++ main.go
@@ -1,17 +1,41 @@
 package main
 
-import "github.com/gin-gonic/gin"
+import (
+	"encoding/json"
+	"fmt"
+	// "github.com/gin-gonic/gin"
+	"io/ioutil"
+	"log"
+)
+
+type Game struct {
+	Id   int    `json:"appid"`
+	Name string `json:name`
+}
+
+var games[]Game
 
 func main() {
 
 	// Gin
-	r := gin.Default()
-	r.GET("/", func(c *gin.Context) {
-		keyword := c.Query("keyword")
-		c.JSON(200, gin.H{
-			"message": keyword,
-		})
-	})
+	// r := gin.Default()
+	// r.GET("/", func(c *gin.Context) {
+	// 	keyword := c.Query("keyword")
+	// 	c.JSON(200, gin.H{
+	// 		"message": keyword,
+	// 	})
+	// })
 
-	r.Run() // Run Gin
+	// JSON
+	bytes, err := ioutil.ReadFile("data/steam_all_apps.json")
+	if err != nil {
+		log.Fatal(err)
+	}
+	if err := json.Unmarshal(bytes, &games); err != nil {
+		log.Fatal(err)
+	} else {
+		fmt.Println("JSON Loaded")
+	}
+
+	// r.Run() // Run Gin
 }
